7edf63b6d6b7a85e708b30ab49fe73c175f517e4
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2023-08-20 20:33:49 +0300

    0. Connecting to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+require("./models/db");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+
+// Import Routers
+
+
+
+// Routers Middlewares
+
+
 
 // Handles any other endpoints [unassigned - endpoints]
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
---
+++ backend/models/db.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB_URI).then(() => {
+    console.log("DB ready to use");
+}).catch((err) => {
+    console.log(err);
+});
