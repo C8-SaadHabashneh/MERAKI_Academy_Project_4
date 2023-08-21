7d88d38bc09977aea85a70d5ebe0f217b2e31cea
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2023-08-21 18:11:32 +0300

    10. Update Profile By Id router

--- backend/routes/profiles.js
+++ backend/routes/profiles.js
@@ -3,12 +3,13 @@ const express = require("express");
 // Import profiles controllers
 const {
     getProfileById,
-    
+    updateProfileById,
 } = require("../controllers/profiles");
 
 // Create profiles Routers
 const profilesRouter = express.Router();
 
 profilesRouter.get("/:id", getProfileById);
+profilesRouter.put("/:id", updateProfileById);
 
 module.exports = profilesRouter;
