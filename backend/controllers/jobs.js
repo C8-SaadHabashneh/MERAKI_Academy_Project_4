0fac880cabacdc6b8a5436f93ec5b06cdc14f9f9
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2023-08-21 04:34:09 +0300

    7. Update Job Post By Id controller

--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -71,8 +71,39 @@ const getJobPostById = (req, res) => {
     });
 };
 
+// this function updates a job post by its id
+const updateJobPostById = (req, res) => {
+    const id = req.params.id;
+    const filter = req.body;
+    Object.keys(filter).forEach((key) => {
+        filter[key].toString().replaceAll(" ", "") == "" && delete filter[key];
+    });
+    jobsModel
+        .findByIdAndUpdate({ _id: id }, req.body, { new: true })
+        .then((newJob) => {
+        if (!newJob) {
+            return res.status(404).json({
+            success: false,
+            message: `The Job post with id => ${id} isn't found`,
+            });
+        }
+        res.status(202).json({
+            success: true,
+            message: "Job post updated",
+            jobPost: newJob,
+        });
+        }).catch((err) => {
+        res.status(500).json({
+            success: false,
+            message: "Server Error",
+            error: err.message,
+        });
+    });
+};
+
 module.exports = {
     createNewJobPost,
     getAllJobPosts,
     getJobPostById,
+    updateJobPostById,
 };
