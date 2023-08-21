e7fbea90dc2932774b1aeb8d98b7f4f5b6c65b94
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2023-08-21 03:42:09 +0300

    5. Get All Job Posts router

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -5,11 +5,12 @@ const {
     createNewJobPost,
     getAllJobPosts,
 } = require("../controllers/jobs");
+const jobs = require("../models/jobs");
 
 // Create jobs Router
 const jobsRouter = express.Router();
 
 jobsRouter.post("/", createNewJobPost);
-
+jobsRouter.get("/", getAllJobPosts);
 
 module.exports = jobsRouter
