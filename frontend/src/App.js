aee821c6703ee832d07ef171b6c9f5b72986e03b
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2023-08-22 18:36:14 +0300

    create navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { AppContext } from "./context";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <AppContext.Provider value>
-      
+        <NavBar />
       </AppContext.Provider>
     </div>
   );
--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./style.css";
 
 const NavBar = () => {
   return (
-    <div>
-
+    <div className='navbar'>
+      <div className='logoDiv'>
+        <Link to="/" className='logoLink'>Recruiter Inc</Link>
+      </div>
+      <div className='navbar-menu'>
+        <Link to="/AllJobs" className='navbar-item'>Jobs</Link>
+        <Link to="/Login" className='navbar-item'>Login</Link>
+        <Link to="/Register" className='navbar-item'>Register</Link>
+      </div>
     </div>
   )
 };
